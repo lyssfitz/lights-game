3e6c6718f95f8dcfc917be991660299541310d44
Lyss <lyssfitz@outlook.com>
2020-02-19 17:32:14 +1100

    Add winner styling

--- src/Board.js
+++ src/Board.js
@@ -91,7 +91,14 @@ class Board extends Component {
 
 	render() {
 		if (this.state.hasWon) {
-			return <h1>YOU WON!</h1>;
+			return (
+				<div className="Board-title">
+					<div className="winner">
+						<span className="neon-orange">YOU</span>
+						<span className="neon-blue">WIN!</span>
+					</div>
+				</div>
+			);
 		}
 		let tblBoard = [];
 		for (let y = 0; y < this.props.nrows; y++) {
